f5243246a3173bad59197a5677ce7730914d2de2
OndraKrejci <o.krejci13@gmail.com>
2021-11-12 23:44:27 +0100

    Zip script

---
+++ zip.sh
@@ -0,0 +1,1 @@
+zip -j xkrejc69.zip calculators/LineMandelCalculator.cc calculators/LineMandelCalculator.h calculators/BatchMandelCalculator.cc calculators/BatchMandelCalculator.h eval.png MB-xkrejc69.txt
